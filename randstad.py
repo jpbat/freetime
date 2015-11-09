44a05493c8c1ff63964a0658599a7af7ef6a3df2
João Ferreira <jooaooferreira@gmail.com>
2015-11-09 15:23:51 +0000

    randstad test

---
+++ randstad.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def solve(numbers):
+
+	numbers.sort(reverse=True)
+
+	index = -1
+
+	for i in xrange(len(numbers)):
+		if numbers[i] % 3 == 0:
+			index = i
+			break
+
+	if index == -1:
+		return 'No solution'
+
+	if index == 0:
+		return 'Solution: ' + str(numbers[0]) + '*' + str(numbers[1]) + ' = ' + str(numbers[0]*numbers[1])
+
+	return 'Solution: ' + str(numbers[0]) + '*' + str(numbers[index]) + ' = ' + str(numbers[0]*numbers[index])
+
+def main():
+	
+	numbers = []
+	info = raw_input('insert numbers: ').split()
+	
+	for l in info:
+		numbers.append(int(l))
+	
+	print solve(numbers)
+
+if __name__ == '__main__':
+	main()
